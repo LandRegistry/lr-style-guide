def1f4cc8e1ee73c8545bb5a867256190c1cc8e0
Demotive <mat@demotive.com>
2015-06-11 11:45:11 +0100

    First commit:
    * Set up basic Flask server
    * Integrate GOV.UK Template
    * GOV.UK Frontend Toolkit as a submodule

---
+++ app/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('layout.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port, debug=True)
