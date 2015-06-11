cf47dbcbbd3f60003de877d0f029cf5d4954f787
Demotive <mat@demotive.com>
2015-06-11 12:23:22 +0100

    Sass and imports working

--- app/server.py
+++ app/server.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template
+from os import path
 
-import os
+from flask.ext.assets import Environment, Bundle
+import sass as libsass
+
+
+__dot = path.dirname(path.realpath(__file__))
+__toolkit_dir = path.join(__dot, 'static/govuk_frontend_toolkit/stylesheets/')
+
+def compile_sass(_in, out, **kw):
+    out.write(
+        libsass.compile(
+            string=_in.read(),
+            include_paths=[__toolkit_dir]
+        )
+    )
+
 
 app = Flask(__name__)
+assets = Environment(app)
+
+sass = Bundle('sass/styleguide.scss',
+              filters=(compile_sass,), output='css/styleguide.css')
+assets.register('styleguide', sass)
+
 
 @app.route('/')
 def home():
