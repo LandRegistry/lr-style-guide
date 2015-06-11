d8ff8acf131e30b8a66982fc107f4f6100775f4b
Demotive <mat@demotive.com>
2015-06-11 14:48:51 +0100

    Adding a sample page for typography

--- app/server.py
+++ app/server.py
@@ -29,10 +29,17 @@ examples = Bundle('sass/styleguide-examples.scss',
               filters=(compile_sass,), output='css/styleguide-examples.css')
 assets.register('styleguide-examples', examples)
 
+# Small thing to allow source code examples in a template
+app.jinja_env.globals['include_raw'] = lambda filename : app.jinja_loader.get_source(app.jinja_env, filename)[0]
+
 
 @app.route('/')
 def home():
     return render_template('styleguide/index.html')
+
+@app.route('/typography')
+def typography():
+    return render_template('styleguide/typography.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
