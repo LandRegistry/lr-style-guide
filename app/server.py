ad27d84a23074dc7ed50525a258a99ec5c40bc07
Demotive <mat@demotive.com>
2015-06-17 11:43:59 +0100

    Basic Heroku setup

--- app/server.py
+++ app/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+
 from os import path
+from os import environ
 
 from flask.ext.assets import Environment, Bundle
 import sass as libsass
@@ -99,7 +101,6 @@ def pagination():
     return render_template('styleguide/pagination.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
