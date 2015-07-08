d59565ccc419fd755e84f10e067728becf5ee8fe
Demotive <mat@demotive.com>
2015-07-08 11:55:00 +0100

    Tweaking and namespacing the print control script

--- lr-styleguide/js/components/buttons-actions.js
+++ lr-styleguide/js/components/buttons-actions.js
@@ -1,12 +1,29 @@
-$(function() {
+(function () {
+  "use strict";
+  var root = this,
+      $ = root.jQuery;
 
-  $('.print-page')
-    .addClass('button-print')
-    .on('click', function(e) {
-      console.log(e.target);
-      e.preventDefault();
-      window.print();
-    })
-    .wrapInner('<a href="#">Print this page</a>');
-  
-});
+  if (typeof LR === 'undefined') { root.LR = {}; }
+
+  var printControls = {
+    _defaultText: 'Print this page',
+
+    init: function(elements) {
+      elements.each(function(index, element) {
+        var element = $(element);
+        if (element.text() === '') {
+          element.text(printControls._defaultText);
+        }
+        element
+          .addClass('button-print')
+          .on('click', function(e) {
+            e.preventDefault();
+            window.print();
+          })
+          .wrapInner('<a href="#"></a>');
+      });
+    }
+  };
+
+  root.LR.printControls = printControls;
+}).call(this);
